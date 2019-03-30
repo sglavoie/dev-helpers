e016ab50d61f84f8d94c1cb452bba45bb3c2442f
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-03-29 21:56:02 -0600

    Fix error with encoded characters

--- zsh_history_cleaner/zsh_history_cleaner.py
+++ zsh_history_cleaner/zsh_history_cleaner.py
@@ -72,7 +72,8 @@ def file_length(file_name):
     '''Return the number of lines in a file. Returns 0 if it doesn't exist.'''
 
     try:
-        with open(file_name) as file_to_check:
+        with open(file_name, errors='replace',
+                  encoding='utf-8') as file_to_check:
             for index, _ in enumerate(file_to_check):
                 pass
             return index + 1  # Will return UnboundLocalError if file is empty
