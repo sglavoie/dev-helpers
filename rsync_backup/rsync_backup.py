c97d4332e0a816ba243b10f101c9b87b2cc23b1b
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-05-13 19:10:37 -0500

    rsync_backup: Move settings to settings.json, refactor

--- rsync_backup/rsync_backup.py
+++ rsync_backup/rsync_backup.py
@@ -1,134 +1,21 @@
 """
-Small script that uses `rsync` to make a simple and convenient backup.
+Script that uses `rsync` to make a simple and convenient backup.
 Note: requires Python 3.6+. No other Python third-party libraries required.
 """
 import argparse
 import datetime
 import glob
+import json
 import os
+import pathlib
 import subprocess
 import sys
 
 
-BACKUP_CMD = ["rsync"]  # This will be used to add options to `rsync` command
-
-###############################################################################
-# SETTINGS
-###############################################################################
-
-# Directories to backup, supplied as a list of strings (no slash at the end)
-DATA_SOURCES = [
-    "/home/sglavoie",
-]
-
-# Single destination of the files to backup, supplied as a string
-# This can be overridden when passing option '-d' or '--dest' to the script
-DATA_DESTINATION = "/media/sglavoie/Elements"
-
-# Line length in the terminal, used for printing separators
-TERMINAL_WIDTH = 40
-
-# Separator to use along with TERMINAL_WIDTH
-SEP = "=-"  # using 2 characters, we have to divide TERMINAL_WIDTH by 2 also
-
-# Sets the prefix of the log filename
-LOG_NAME = ".backup_log_"
-
-# This goes right after LOG_NAME as a suffix
-LOG_FORMAT = "%y%m%d_%H_%M_%S"
-
-# Options to use with rsync as a list of strings
-RSYNC_OPTIONS = [
-    "-vaAHh",
-    "--delete",
-    "--ignore-errors",
-    "--force",
-    "--prune-empty-dirs",
-    "--delete-excluded",
-]
-
-BACKUP_CMD.extend(RSYNC_OPTIONS)  # adds options set above
-
-# Default file in each source in DATA_SOURCES where files/directories
-# will be ignored
-BACKUP_EXCLUDE = ".backup_exclude"
-
-###############################################################################
-# FUNCTIONS
-###############################################################################
-
-
-def better_separation(the_function, sep=SEP, term_width=TERMINAL_WIDTH):
-    """Decorator used to print separators around `the_function`."""
-
-    def print_separator(*args, **kwargs):
-        """Surrounds `the_function` with a separator and add a new line."""
-        separator = sep * term_width
-        print(separator)
-        the_function(*args, **kwargs)
-        print(separator, "\n")
-
-    return print_separator
-
-
-@better_separation
-def backing_source(
-    source: str, backup_source: list, log_filename: str
-) -> None:
-    """Print information to STDOUT and to `log_filename` and executes the
-    rsync command."""
-    cmd_executed = " ".join(backup_source)
-    msg_executed = f"Command executed:\n{cmd_executed}\n"
-    print(msg_executed)
-
-    with open(log_filename, mode="w") as log_file:
-        log_file.write(f"{msg_executed}\n")
-
-    child = subprocess.Popen(backup_source)
-    _ = child.communicate()[0]  # call communicate to get the return code
-    rc = child.returncode
-
-    print(f"\nBackup completed for: {source} (return code: {rc})")
-
-
-def user_says_yes(
-    msg: str = "\nDo you want to delete log files for this source? (y/n) ",
-) -> bool:
-    """Asks the user to enter either "y" or "n" to confirm. Returns boolean."""
-    choice = None
-    while choice is None:
-        user_input = input(msg)
-        if user_input.lower() == "y":
-            choice = True
-        elif user_input.lower() == "n":
-            choice = False
-        else:
-            print('Please enter either "y" or "n".')
-    return choice
-
-
-def clear_logs(*args) -> None:
-    """Clears log files for each source specified in SETTINGS."""
-    for source in args:
-        # Retrieve a list of all matching log files in `source`
-        log_files = glob.glob(f"{source}/{LOG_NAME}*")
-        if log_files == []:
-            print(f"\nThere is no log file to delete in {source}.")
-            sys.exit(0)
-        else:
-            print(f"Log files in {source}:")
-            for log_file in log_files:
-                print(log_file)
-            if user_says_yes():
-                for log_file in log_files:
-                    os.remove(log_file)
-                print("Log files deleted.")
-        print("Exiting script...")
-        sys.exit(0)
-
-
-def run_backup(*args, data_destination: str = DATA_DESTINATION):
+def run_backup():
     """This is where all the action happens!"""
+    settings = get_settings()
+
     # initiate the parser
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -151,9 +38,13 @@ def run_backup(*args, data_destination: str = DATA_DESTINATION):
 
     # check for --clear or -c
     if arguments.clear:
-        clear_logs(*args)
+        clear_logs(
+            data_sources=settings["data_sources"],
+            log_name=settings["log_name"],
+        )
 
     # check for --dest or -d
+    data_destination = settings["data_destination"]
     if arguments.destination is not None:
         if os.path.isdir(arguments.destination):
             data_destination = arguments.destination
@@ -166,17 +57,19 @@ def run_backup(*args, data_destination: str = DATA_DESTINATION):
         print(f"The destination doesn't exist.\n({data_destination})")
         sys.exit(0)
 
-    for source in args:
+    for source in settings["data_sources"]:
         date_now = datetime.datetime.now()
-        log_format = datetime.datetime.strftime(date_now, LOG_FORMAT)
-        log_filename = f"{source}/{LOG_NAME}{log_format}"
+        log_format = datetime.datetime.strftime(
+            date_now, settings["log_format"]
+        )
+        log_filename = f"{source}/{settings['log_name']}{log_format}"
         log_option = f"--log-file={log_filename}"
 
-        backup_source = BACKUP_CMD.copy()
+        backup_source = settings["backup_cmd"].copy()
         backup_source.extend([log_option])
 
         # files to ignore in backup
-        exclude_file = f"{source}/{BACKUP_EXCLUDE}"
+        exclude_file = f"{source}/{settings['backup_exclude']}"
 
         if os.path.exists(exclude_file):
             exclude_option = f"--exclude-from={exclude_file}"
@@ -185,12 +78,85 @@ def run_backup(*args, data_destination: str = DATA_DESTINATION):
             # skips '--exclude-from' option if no file is found
             backup_source.extend([source, data_destination])
 
-        backing_source(source, backup_source, log_filename)
+        settings["source"] = source
+        settings["backup_source"] = backup_source
+        settings["log_filename"] = log_filename
+        backing_source(settings)
 
 
-###############################################################################
-# EXECUTION
-###############################################################################
+def backing_source(settings: dict) -> None:
+    """Print information to STDOUT and to `log_filename` and executes the
+    rsync command."""
+    print(settings["sep"] * settings["terminal_width"])
+
+    cmd_executed = " ".join(settings["backup_source"])
+    msg_executed = f"Command executed:\n{cmd_executed}\n"
+    print(msg_executed)
+
+    with open(settings["log_filename"], mode="w") as log_file:
+        log_file.write(f"{msg_executed}\n")
+
+    child = subprocess.Popen(settings["backup_source"])
+    _ = child.communicate()[0]  # call communicate to get the return code
+    rc = child.returncode
+
+    print(f"\nBackup completed for: {settings['source']} (return code: {rc})")
+    print(settings["sep"] * settings["terminal_width"])
+
+
+def clear_logs(data_sources: list, log_name: str) -> None:
+    """Clears log files for each source specified in SETTINGS."""
+    for source in data_sources:
+        # Retrieve a list of all matching log files in `source`
+        log_files = glob.glob(f"{source}/{log_name}*")
+        if log_files == []:
+            print(f"\nThere is no log file to delete in {source}.")
+            sys.exit(0)
+        else:
+            print(f"Log files in {source}:")
+            for log_file in log_files:
+                print(log_file)
+            if user_says_yes():
+                for log_file in log_files:
+                    os.remove(log_file)
+                print("Log files deleted.")
+        print("Exiting script...")
+        sys.exit(0)
+
+
+def user_says_yes(
+    msg: str = "\nDo you want to delete log files for this source? (y/n) ",
+) -> bool:
+    """Asks the user to enter either "y" or "n" to confirm. Returns boolean."""
+    choice = None
+    while choice is None:
+        user_input = input(msg)
+        if user_input.lower() == "y":
+            choice = True
+        elif user_input.lower() == "n":
+            choice = False
+        else:
+            print('Please enter either "y" or "n".')
+    return choice
+
+
+def get_settings() -> dict:
+    """
+    Get the settings from `settings.json`.
+
+    Returns:
+        dict: Containing all settings used by the tool.
+    """
+    directory = pathlib.Path(__file__).parent.absolute()
+    with open(directory / "settings.json") as fp:
+        settings = json.load(fp)
+
+    backup_cmd = ["rsync"]
+    backup_cmd.extend(settings["rsync_options"])
+    settings["backup_cmd"] = backup_cmd
+
+    return settings
+
 
 if __name__ == "__main__":
-    run_backup(*DATA_SOURCES)
+    run_backup()
