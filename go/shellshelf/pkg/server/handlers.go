a21e53c82c4e55b613f42f9be536162a48473e96
sglavoie <sgdlavoie@gmail.com>
2023-11-26 18:41:26 -0600

    Break up templates into sub-templates and import from them

--- go/shellshelf/pkg/server/handlers.go
+++ go/shellshelf/pkg/server/handlers.go
@@ -10,8 +10,8 @@ import (
 // IndexHandler creates a new HTTP handler function with access to the given Config.
 func IndexHandler(cfg *models.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
-		err := tmpl.Execute(w, cfg.Commands)
+		tmpl := template.Must(template.ParseGlob("web/templates/*.html"))
+		err := tmpl.ExecuteTemplate(w, "index.html", cfg.Commands)
 		if err != nil {
 			fmt.Println("Error executing template:", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
