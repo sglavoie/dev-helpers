fb3802cb923875a69469838e0b307fe771edf7bd
Sébastien Lavoie <sgdlavoie@gmail.com>
2024-05-19 10:56:54 -0600

    photos_backup: set up `sdd` command to back up SD card and Apple Photos

--- python/photos_backup/src/photos_backup/sdd/backup.py
+++ python/photos_backup/src/photos_backup/sdd/backup.py
@@ -7,43 +7,50 @@ from pathlib import Path
 class Backup:
     def __init__(self, dry_run: bool) -> None:
         self.dry_run = dry_run
-        self.src_path: Path = Path()
-        self.dst_path: Path = Path()
+        self.apple_photos_path: Path = Path()
+        self.sd_card_path: Path = Path()
+        self.sdd_dst_path: Path = Path()
         self._set_up_paths()
 
     def backup(self) -> None:
         dry_run = "--dry-run" if self.dry_run else ""
         cmd = f"""rsync -a --progress \
             {dry_run} \
-            {self.src_path} {self.dst_path}"""
+            {self.apple_photos_path} {self.sdd_dst_path}"""
+        subprocess.run(shlex.split(cmd), check=True)
+
+        cmd = f"""rsync -a --progress \
+            {dry_run} \
+            {self.sd_card_path} {self.sdd_dst_path}"""
         subprocess.run(shlex.split(cmd), check=True)
 
     def _set_up_paths(self) -> None:
-        src_path = "SDD_SRC_PATH"
-        dst_path = "SDD_DST_PATH"
-        sdd_src_path = os.getenv(src_path, "")
-        sdd_dst_path = os.getenv(dst_path, "")
+        apple_photos_path_env = "APPLE_PHOTOS_DST_PATH"
+        apple_photos_path = os.getenv(apple_photos_path_env, "")
+        sd_card_path_env = "SD_CARD_DST_PATH"
+        sd_card_path = os.getenv(sd_card_path_env, "")
+        sdd_dst_path_env = "SDD_DST_PATH"
+        sdd_dst_path = os.getenv(sdd_dst_path_env, "")
 
-        for env_var_name, env_var_value in {
-            src_path: {"type": "source", "path": sdd_src_path},
-            dst_path: {"type": "destination", "path": sdd_dst_path},
-        }.items():
-            if not env_var_value:
-                raise ValueError(f"'{env_var_name}' is not set")
+        for path_env, path_value in zip(
+            [apple_photos_path_env, sd_card_path_env, sdd_dst_path_env],
+            [apple_photos_path, sd_card_path, sdd_dst_path],
+        ):
+            if not path_value:
+                raise ValueError(f"'{path_env}' is not set")
 
-            if not os.path.exists(env_var_value.get("path", "")):
-                if env_var_value.get("type") == "source":
-                    raise FileNotFoundError(
-                        f"{env_var_name}={env_var_value} does not exist"
-                    )
+        for path in [apple_photos_path, sd_card_path]:
+            if not os.path.exists(path):
+                raise FileNotFoundError(f"'{path}' does not exist")
 
-                if env_var_value.get("type") == "destination":
-                    try:
-                        os.makedirs(env_var_value.get("path", ""), exist_ok=True)
-                    except OSError as e:
-                        raise OSError(
-                            f"Could not create {env_var_value.get('type')} directory: {env_var_value.get('path')}"
-                        ) from e
+        if not os.path.exists(sdd_dst_path):
+            try:
+                os.makedirs(sdd_dst_path, exist_ok=True)
+            except OSError as e:
+                raise OSError(
+                    f"Could not create {sdd_dst_path_env} directory: {sdd_dst_path}"
+                ) from e
 
-        self.src_path = Path(sdd_src_path)
-        self.dst_path = Path(sdd_dst_path)
+        self.apple_photos_path = Path(apple_photos_path)
+        self.sd_card_path = Path(sd_card_path)
+        self.sdd_dst_path = Path(sdd_dst_path)
